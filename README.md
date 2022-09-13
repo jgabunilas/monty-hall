4fc454181c7b90c8b1239cf9fb45ed27bacb1406
unknown <jgabunilas@gmail.com>
2022-09-12 20:00:18 -0700

    README and Code Updates

--- README.md
+++ README.md
@@ -33,16 +33,16 @@ The debate and discussion surrounding the problem stems from whether the player 
 Part 1 of this exercise is a functional implementation of the game that walks the player through the six steps presented above. The manual game is launched by selecting option 1 when prompted by the game. 
 
 The basic outline of the program is as follows:
-1. The three prizes (two donkeys and a car) are stored in a list called `choices`. This list is shuffled using the `random.shuffle()` function
+1. The three prizes (two goats and a car) are stored in a list called `choices`. This list is shuffled using the `random.shuffle()` function
 2. The player is prompted to selected either door 1, 2 ,or 3 using the `input()` function. Data validation ensures choices are limited to one of these three entries.
 3.  Once the player has selected a door, the program then constructs a new list called `other_doors` that contains the numbers of the two doors that were *not* selected by the player (for example, if the player chose door 1, then `other_doors` will contain doors 2 and 3).  The `other_doors` list is then shuffled (see below).
-4.  The `other_doors` list is used to "check" the unchosen doors and open one that has a donkey. If the first door in the list (at index 0) has a goat, that door is opened and the goat is revealed to the player. If the first door in the list has a car, then the second door in the list (at index 1) is opened and the goat is revealed to the player.
+4.  The `other_doors` list is used to "check" the unchosen doors and open one that has a goat. If the first door in the list (at index 0) has a goat, that door is opened and the goat is revealed to the player. If the first door in the list has a car, then the second door in the list (at index 1) is opened and the goat is revealed to the player.
     * This iterative checking procedure illustrates the need to shuffle the `other_doors` list. The unchosen door that is opened must **always** reveal a goat. If this list is not shuffled and yet used iteratively (incrementally from index 0 onward), then the attentive player will begin to realize patterns of which doors are opened based on his or her original selection.
     * For example, when a player chooses door 1:
-        * If the car is behind door 1, then both doors 2 and 3 have donkeys. Since the list is checked iteratively, then in an unshuffled list, door 2 will always be checked and opened first.
-        * Thus, over many iterations, a player may realize that whenever door 2 is opened to reveal a donkey, there is a higher-than-average chance that door 1 has a car. By randomizing the check, there is a chance to check door 3 for a donkey first. This is much less revealing.
+        * If the car is behind door 1, then both doors 2 and 3 have goats. Since the list is checked iteratively, then in an unshuffled list, door 2 will always be checked and opened first.
+        * Thus, over many iterations, a player may realize that whenever door 2 is opened to reveal a goat, there is a higher-than-average chance that door 1 has a car. By randomizing the check, there is a chance to check door 3 for a goat first. This is much less revealing.
 5. The player is prompted via `input()` to choose whether to switch to the remaining unopened door ("y") or stay with the original chosen door ("n"). Again, data validation ensures the integrity of the entry.
-6. The player final door that the player has chosen is opened, and the prize is revealed. Additionally, the player is informed of whether a car or a donkey was behind the door that was selected at the beginning of the game.
+6. The player final door that the player has chosen is opened, and the prize is revealed. Additionally, the player is informed of whether a car or a goat was behind the door that was selected at the beginning of the game.
 
 ![](manual_play.jpg)
 
@@ -55,18 +55,18 @@ The basic outline of the program is as follows:
 2. The player is next prompted via `input()` to choose whether the simulation will switch doors ("y") or remain with the originally-selected door ("n"). The value is stored as the varaible `switch`.
 3. Once input is received, an empty `results` list is created that will eventually store the result of each simulation that is run.
 4. Next, for each simulation that is run, the program executes the function `run_sim()`. This function accepts the `switch` variable as an argument and performs the following:
-    * Creates a list called `choices` which holds the three prizes (one car and two donkeys).
+    * Creates a list called `choices` which holds the three prizes (one car and two goats).
     * A prize within `choices` is chosen at random using `random.choice`, and is stored as the variable `initial_choice`.
-    * The randomly-chosen prize (car or donkey) is removed from the `choices` list.
-    * Next, one donkey is removed from the `choices` list to simulate the opening of the unchosen door to revealed a donkey. Thereafter, the `choices` list will contain just one final item, which is saved as the variable `remaining_choice`.
+    * The randomly-chosen prize (car or goat) is removed from the `choices` list.
+    * Next, one goat is removed from the `choices` list to simulate the opening of the unchosen door to revealed a goat. Thereafter, the `choices` list will contain just one final item, which is saved as the variable `remaining_choice`.
     * If the player directed the simulator to switch doors, the variable `prize` will be set equal to `remaining_choice`. If the player directed the simulator to stay with the initial door, then the `prize` variable will be set equal to `initial_choice`.
     * The function returns a tuple containing `initial_choice` and `prize`.
 5. The returned value of the `run_sim()` function call is appended to the `results` list.
 6. After all simulations have been executed, the program iterates through the the `results` list and constructs a `final_prizes` list using the `prize` values from each tuple in `results`, which is indexed as position 1.
-7. The program iterates through the `final_prizes` list and sums the total number of donkeys and cars. It stores these sums as `total_donkey_prizes` and `total_car_prizes`, respectively. It also calculates the percentage of prizes are donkeys and cars, stored these as `percent_donkeys` and `percent_cars`.
-8. The program prints out the total numbers and percentages of car and donkey prizes.
+7. The program iterates through the `final_prizes` list and sums the total number of goats and cars. It stores these sums as `total_goat_prizes` and `total_car_prizes`, respectively. It also calculates the percentage of prizes are goats and cars, stored these as `percent_goats` and `percent_cars`.
+8. The program prints out the total numbers and percentages of car and goat prizes.
 9. The program creates a Pandas dataframe with **Total Prizes** and **Percent**.
-10. The program creates a simple bar graph that plots the number of car and donkey prizes based on the number of simulations run.
+10. The program creates a simple bar graph that plots the number of car and goat prizes based on the number of simulations run.
 
 ![](simulation.jpg)
 
--- monty_hall.py
+++ monty_hall.py
@@ -14,14 +14,14 @@ while True:
     else:
         print("You must choose either 1 or 2.")
 
-# Create a function that runs a simulation of the Monty Hall problem. The only parameter that it takes is "switch", which determines whether the simulator switches doors after the unchosen "donkey door" is opened. The function returns a tuple containing the initial choice.
+# Create a function that runs a simulation of the Monty Hall problem. The only parameter that it takes is "switch", which determines whether the simulator switches doors after the unchosen "goat door" is opened. The function returns a tuple containing the initial choice and the ultimate prize.
 
 def run_sim(switch):
-    choices = ['car','donkey','donkey']
+    choices = ['car','goat','goat']
     initial_choice = random.choice(choices)
     # print(f"Initial choice: {initial_choice}")
     choices.remove(initial_choice)
-    choices.remove('donkey')
+    choices.remove('goat')
     remaining_choice = choices[0]
     # print(f"Remaining choice: {remaining_choice}")
 
@@ -37,54 +37,54 @@ if int(playstyle) == 1:
     print("Welcome to Let's Make a Deal!")
 
     # Create the choices (doors) and randomly shuffle them
-    choices = ['donkey','donkey','car']
+    choices = ['goat','goat','car']
     random.shuffle(choices)
     # DEV ONLY: print the shuffled choices
     # 1print(choices)
 
     # Ask the player to choose a door
     while True:
-        choice = input("Before you are three doors. Behind one of them is a new car. Behind the other two is a donkey. You will win whatever is behind the door you choose! Please choose a door (1, 2, or 3): ")
+        choice = input("Before you are three doors. Behind one of them is a new car. Behind the other two is a goat. You will win whatever is behind the door you choose! Please choose a door (1, 2, or 3): ")
         if choice == "1" or choice == "2" or choice == "3":
             break
         else:
             print("Your choice must be either 1, 2 or 3.")
-        # DEV ONLY: indicate which prize is behind the door (car or donkey). Remember that the actual prize is indexed at one position lower than the door number choice.
+        # DEV ONLY: indicate which prize is behind the door (car or goat). Remember that the actual prize is indexed at one position lower than the door number choice.
         # print(f'Chosen prize: {choices[int(choice) - 1]}')
     # Inform the player which door they have chosen.
     print(f'You have chosen Door #{choice}')
-    # Open one of the non-selected doors. If a car is behind the selected door, then one of the two remaining doors with a donkey will be opened. If a donkey is behind the selected door, then the sole remaining door with a donkey will be opened. 
+    # Open one of the non-selected doors. If a car is behind the selected door, then one of the two remaining doors with a goat will be opened. If a goat is behind the selected door, then the sole remaining door with a goat will be opened. 
     print(f"Before we open Door #{choice}, let's open one of the doors that you did not choose!")
     if int(choice) == 1:
         # If door 1 was selected, then we must open either door 2 or door 3. 
-        # randomize which of the remaining doors 2 and 3 will be "checked" first. Remember that doors 2 and 3 are indexed at position 1 and 2 of the choices list, respectively. Therefore, we create a list of the remaining doors, shuffle that list, then check the first door (index 0) of the shuffled list. If that door has a donkey, open it. If it does not, then open the door at index 1.
+        # randomize which of the remaining doors 2 and 3 will be "checked" first. Remember that doors 2 and 3 are indexed at position 1 and 2 of the choices list, respectively. Therefore, we create a list of the remaining doors, shuffle that list, then check the first door (index 0) of the shuffled list. If that door has a goat, open it. If it does not, then open the door at index 1.
         other_doors = [1, 2]
         random.shuffle(other_doors)
-        if choices[other_doors[0]] == 'donkey':
-            print(f"We have opened Door #{other_doors[0] + 1}, which reveals a donkey! Good job dodging that one!")
+        if choices[other_doors[0]] == 'goat':
+            print(f"We have opened Door #{other_doors[0] + 1}, which reveals a goat! Good job dodging that one!")
             remaining_door = other_doors[1]
         else:
-            print(f"We have opened Door #{other_doors[1] + 1}, which reveals a donkey! Good job dodging that one!")
+            print(f"We have opened Door #{other_doors[1] + 1}, which reveals a goat! Good job dodging that one!")
             remaining_door = other_doors[0]
     elif int(choice) == 2:
         # The same logic applies if door 2 was chosen. The only change is that the other_doors list will have doors 1 and 3 (indexed at 0 and 2 respectively).
         other_doors = [0, 2]
         random.shuffle(other_doors)
-        if choices[other_doors[0]] == 'donkey':
-            print(f"We have opened Door #{other_doors[0] + 1}, which reveals a donkey! Good job dodging that one!")
+        if choices[other_doors[0]] == 'goat':
+            print(f"We have opened Door #{other_doors[0] + 1}, which reveals a goat! Good job dodging that one!")
             remaining_door = other_doors[1]
         else:
-            print(f"We have opened Door #{other_doors[1] + 1}, which reveals a donkey! Good job dodging that one!")
+            print(f"We have opened Door #{other_doors[1] + 1}, which reveals a goat! Good job dodging that one!")
             remaining_door = other_doors[0]
     elif int(choice) == 3:
         # The same logic applies for door 3. The only change is that the other_doors list will have doors 1 and 2 (indexed at 0 and 1 respectively).
         other_doors = [0, 1]
         random.shuffle(other_doors)
-        if choices[other_doors[0]] == 'donkey':
-            print(f"We have opened Door #{other_doors[0] + 1}, which reveals a donkey! Good job dodging that one!")
+        if choices[other_doors[0]] == 'goat':
+            print(f"We have opened Door #{other_doors[0] + 1}, which reveals a goat! Good job dodging that one!")
             remaining_door = other_doors[1]
         else:
-            print(f"We have opened Door #{other_doors[1] + 1}, which reveals a donkey! Good job dodging that one!")
+            print(f"We have opened Door #{other_doors[1] + 1}, which reveals a goat! Good job dodging that one!")
             remaining_door = other_doors[0]
     while True:
         switch = input(f'Would you like to switch to Door #{remaining_door + 1} (y/n)?: ')
@@ -117,50 +117,59 @@ elif int(playstyle) == 2:
             print("You must enter an integer number.")
 
     while True:
-        switch = input("Please indicate whether the simulator will switch to the remaining door (y/n) ")
+        switch = input("Please indicate whether the simulator will switch to the remaining door after one of the unchosen doors is opened (y/n) ")
         if switch == 'y' or switch == 'n':
             break
         else:
             print('You must enter "y" or "n".')
 
+    # Initialize a list that will contain the results of each simulation
     results = []
+
+    # Run the simulation num_sims times
     num_sims = int(num_sims)
     for n in range(num_sims):
         results.append(run_sim(switch))
 
     # print(results)
+
+    # Create a list of initial choices, which is the first (0-indexed) object of each simulation result tuple in the results list
     initial_choices = []
     for result in results:
         initial_choices.append(result[0])
     # print(initial_choices)
 
+    # Count the number of times that the simulator selected a car first, or a goat first
     initial_cars = 0
-    initial_donkeys = 0
+    initial_goats = 0
     for choice in initial_choices:
-        if choice == 'donkey':
-            initial_donkeys += 1
+        if choice == 'goat':
+            initial_goats += 1
         elif choice == "car":
             initial_cars += 1
 
+    # Createa a list of final prices, which is the second (1-indexed) onject of each simulation result tuple in the results list
     final_prizes = []
     for result in results:
         final_prizes.append(result[1])
     # print(final_prizes)
 
-    total_donkey_prizes = 0
+    # Calculate the percentage of final prizes that are cars and goats
+    total_goat_prizes = 0
     total_car_prizes = 0
     for prize in final_prizes:
-        if prize == "donkey":
-            total_donkey_prizes += 1
-            percent_donkeys = 100 * total_donkey_prizes / num_sims
+        if prize == "goat":
+            total_goat_prizes += 1
+            percent_goats = 100 * total_goat_prizes / num_sims
         elif prize == "car":
             total_car_prizes += 1
             percent_cars = 100 * total_car_prizes / num_sims
     
     print(f"Total car prizes: {total_car_prizes} ({percent_cars}%)")
-    print(f"Total donkey prizes: {total_donkey_prizes} ({percent_donkeys})%")
+    print(f"Total goat prizes: {total_goat_prizes} ({percent_goats})%")
 
-    df = pd.DataFrame(data = {'Total Prizes': [total_car_prizes, total_donkey_prizes], 'Percent':[str(percent_cars) + "%", str(percent_donkeys) + "%"]}, index = ["Cars", "Donkeys"])
+    # Create a Pandas dataframe containing the total number of car and goat prizes, and the percentages of each type of prize
+    df = pd.DataFrame(data = {'Total Prizes': [total_car_prizes, total_goat_prizes], 'Percent':[str(percent_cars) + "%", str(percent_goats) + "%"]}, index = ["Cars", "Goats"])
     # print(df)
 
     ax = df.plot(kind = 'bar', title = f"Prize Results for {num_sims} Simulations, switch = {switch}")
@@ -171,8 +180,8 @@ elif int(playstyle) == 2:
     # # Remove the chosen item from the list
     # print(choices)
 
-    # # Remove a donkey from the list
-    # choices.remove('donkey')
+    # # Remove a goat from the list
+    # choices.remove('goat')
     # print(choices)
 
     # switch = input('Would you like to switch to the remaining door (y/n)? ')
